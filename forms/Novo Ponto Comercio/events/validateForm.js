9b7a9e1be2e57a87a9d22d5205e1e744e6cbf2bc
maykon3 <mkn.silva29@gmail.com>
2025-04-15 09:58:57 -0300

    style: mudança de layout

--- forms/Novo Ponto Comercio/events/validateForm.js
+++ forms/Novo Ponto Comercio/events/validateForm.js
@@ -1,23 +1,24 @@
-function validateForm(form){
-var msg = "";
+function validateForm(form) {
+    var msg = "";
 
-    if (form.getValue("nome") == ""){
-        msg+= i18n.translate("nome") + ", ";
-    }
+    if (form.getValue("nome") == "") {
+        msg += i18n.translate("nome") + ", ";
 
-    if (form.getValue("email")  == ""){
-        msg+= i18n.translate("email")+ ", ";
     }
 
-    if (form.getValue("telefone")  == ""){
-        msg+= i18n.translate("telofone")+ ", ";
+    if (form.getValue("email") == "") {
+        msg += i18n.translate("email") + ", ";
     }
-    if (form.getValue("cep")  == ""){
-        msg+= i18n.translate("CEP")+ ", ";
+
+    if (form.getValue("telefone") == "") {
+        msg += i18n.translate("telofone") + ", ";
     }
-    if (msg != ""){
-        msg = i18n.translate("campos")  + msg;
-         throw msg;
+    if (form.getValue("cep") == "") {
+        msg += i18n.translate("CEP") + ", ";
     }
-    
+    if (msg != "") {
+        msg = i18n.translate("campos") + msg;
+        throw msg;
+    }
+
 }
