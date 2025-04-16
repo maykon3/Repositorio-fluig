cbc968bbdb8e89338c6bf9aee33cb190ac30c17d
maykon3 <mkn.silva29@gmail.com>
2025-04-16 15:44:21 -0300

    feat: mecanismo de atribuição no formulario e no diagrama

---
+++ mechanisms/CursomAprovM.js
@@ -0,0 +1,12 @@
+function resolve(process,colleague){
+
+	var userList = new java.util.ArrayList();
+	
+	 var pais =  "Aprovadores_" + hAPI.getCardValue("pais");
+	 
+
+	userList.add("Pool:Group:" + pais);
+
+	return userList;
+
+}
